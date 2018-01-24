e74d497e959342cbd9f756abd1b1f722db465eae
Shakhawat Hossen <shakil.sub.cse@gmail.com>
2018-01-25 00:09:37 +0600

    modify

--- app/src/main/java/com/example/shakil/restaurantfoodorder/FoodList.java
+++ app/src/main/java/com/example/shakil/restaurantfoodorder/FoodList.java
@@ -3,6 +3,7 @@ package com.example.shakil.restaurantfoodorder;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +11,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.shakil.restaurantfoodorder.Common.Common;
@@ -23,10 +26,12 @@ import com.facebook.share.model.SharePhoto;
 import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareDialog;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.squareup.picasso.Picasso;
@@ -222,13 +227,16 @@ public class FoodList extends AppCompatActivity {
     }
 
     private void startSearch(CharSequence text) {
-        searchAdapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(Food.class,
-                R.layout.food_item,
-                FoodViewHolder.class,
-                //compare name
-                foodList.orderByChild("Name").equalTo(text.toString())) {
+
+        //Create query by name
+        Query searchByName = foodList.orderByChild("Name").equalTo(text.toString());
+
+        //Create Options with query
+        FirebaseRecyclerOptions<Food> foodOptions = new FirebaseRecyclerOptions.Builder<Food>().setQuery(searchByName, Food.class).build();
+
+        searchAdapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(foodOptions) {
             @Override
-            protected void populateViewHolder(FoodViewHolder viewHolder, Food model, int position) {
+            protected void onBindViewHolder(@NonNull FoodViewHolder viewHolder, int position, @NonNull Food model) {
                 viewHolder.food_name.setText(model.getName());
                 Picasso.with(getBaseContext()).load(model.getImage()).into(viewHolder.food_image);
 
@@ -243,7 +251,14 @@ public class FoodList extends AppCompatActivity {
                     }
                 });
             }
+
+            @Override
+            public FoodViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.food_item,parent, false);
+                return new FoodViewHolder(itemView);
+            }
         };
+        searchAdapter.startListening();
         recyclerView.setAdapter(searchAdapter); //Set adapter for recycler view is search result
     }
 
@@ -265,16 +280,21 @@ public class FoodList extends AppCompatActivity {
     }
 
     private void loadListFood(String categoryId) {
-        adapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(Food.class, R.layout.food_item, FoodViewHolder.class,
-                foodList.orderByChild("menuId").equalTo(categoryId)) {
 
-            //like: Select * from Foods where MenuId
+        //Create query by category Id
+        Query searchByName = foodList.orderByChild("menuId").equalTo(categoryId);
 
+        //Create Options with query
+        FirebaseRecyclerOptions<Food> foodOptions = new FirebaseRecyclerOptions.Builder<Food>().setQuery(searchByName, Food.class).build();
+
+
+        adapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(foodOptions) {
             @Override
-            protected void populateViewHolder(final FoodViewHolder viewHolder, final Food model, final int position) {
+            protected void onBindViewHolder(@NonNull final FoodViewHolder viewHolder, final int position, @NonNull final Food model) {
+
                 viewHolder.food_name.setText(model.getName());
                 viewHolder.food_price.setText(String.format("$ %s", model.getPrice().toString()));
-                /*Log.d("TAG", model.getName());*/
+
                 Picasso.with(getBaseContext()).load(model.getImage()).into(viewHolder.food_image);
 
 
@@ -321,9 +341,23 @@ public class FoodList extends AppCompatActivity {
                     }
                 });
             }
+
+            @Override
+            public FoodViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.food_item,parent, false);
+                return new FoodViewHolder(itemView);
+            }
         };
+        adapter.startListening();
 
         recyclerView.setAdapter(adapter);
         swipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+        searchAdapter.stopListening();
     }
 }
--- app/src/main/java/com/example/shakil/restaurantfoodorder/OrderStatus.java
+++ app/src/main/java/com/example/shakil/restaurantfoodorder/OrderStatus.java
@@ -1,16 +1,24 @@
 package com.example.shakil.restaurantfoodorder;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.shakil.restaurantfoodorder.Common.Common;
+import com.example.shakil.restaurantfoodorder.Model.Food;
 import com.example.shakil.restaurantfoodorder.Model.Request;
+import com.example.shakil.restaurantfoodorder.ViewHolder.FoodViewHolder;
 import com.example.shakil.restaurantfoodorder.ViewHolder.OrderViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class OrderStatus extends AppCompatActivity {
 
@@ -47,20 +55,35 @@ public class OrderStatus extends AppCompatActivity {
     }
 
     private void loadOrders(String phone) {
-        adapter = new FirebaseRecyclerAdapter<Request, OrderViewHolder>(Request.class,
-                R.layout.order_layout,
-                OrderViewHolder.class,
-                requests.orderByChild("phone").equalTo(phone)) {
+
+        Query getOrderByUser = requests.orderByChild("phone").equalTo(phone);
+
+        //Create Options with query
+        FirebaseRecyclerOptions<Request> orderOptions = new FirebaseRecyclerOptions.Builder<Request>().setQuery(getOrderByUser, Request.class).build();
+
+
+        adapter = new FirebaseRecyclerAdapter<Request, OrderViewHolder>(orderOptions) {
             @Override
-            protected void populateViewHolder(OrderViewHolder viewHolder, Request model, int position) {
+            protected void onBindViewHolder(@NonNull OrderViewHolder viewHolder, int position, @NonNull Request model) {
                 viewHolder.txtOrderId.setText(adapter.getRef(position).getKey());
                 viewHolder.txtOrderStatus.setText(Common.convertCodeToStatus(model.getStatus()));
                 viewHolder.txtOrderAddress.setText(model.getAddress());
                 viewHolder.txtOrderPhone.setText(model.getPhone());
             }
+
+            @Override
+            public OrderViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.food_item,parent, false);
+                return new OrderViewHolder(itemView);
+            }
         };
+        adapter.startListening();
         recyclerView.setAdapter(adapter);
     }
 
-
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
 }
